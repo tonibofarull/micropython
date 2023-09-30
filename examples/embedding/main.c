d93e25484ca1d811657f4f3c3d216395a8a98396
tonibofarull <toni.bofarull@midokura.com>
2023-09-30 10:17:06 +0200

    compile examples/embedding with wasi-sdk

--- examples/embedding/main.c
+++ examples/embedding/main.c
@@ -3,39 +3,41 @@
  * Copyright (c) 2022-2023 Damien P. George
  */
 
+#include <stdio.h>
 #include "port/micropython_embed.h"
 
 // This is example 1 script, which will be compiled and executed.
 static const char *example_1 =
-    "print('hello world!', list(x + 1 for x in range(10)), end='eol\\n')";
-
-// This is example 2 script, which will be compiled and executed.
-static const char *example_2 =
-    "for i in range(10):\n"
-    "    print('iter {:08}'.format(i))\n"
-    "\n"
-    "try:\n"
-    "    1//0\n"
-    "except Exception as er:\n"
-    "    print('caught exception', repr(er))\n"
-    "\n"
-    "import gc\n"
-    "print('run GC collect')\n"
-    "gc.collect()\n"
-    "\n"
-    "print('finish')\n"
-    ;
+"a = [2**x for x in range(10)]\n"
+"print(a)\n"
+"\n"
+"def fib(n):\n"
+"    if n <= 0:\n"
+"        return 0\n"
+"    if n <= 2:\n"
+"        return 1\n"
+"    return fib(n-1)+fib(n-2)\n"
+"print(fib(5))\n"
+"print(fib(19))\n"
+"class A:\n"
+"    def __init__(self):\n"
+"        self.x = fib(19)\n"
+"a = A()\n"
+"print(a.x+1)"
+;
 
 // This array is the MicroPython GC heap.
 static char heap[8 * 1024];
 
 int main() {
     // Initialise MicroPython.
+    // printf("here\n"); fflush(stdout);
     mp_embed_init(&heap[0], sizeof(heap));
-
+    // printf("here\n"); fflush(stdout);
     // Run the example scripts (they will be compiled first).
     mp_embed_exec_str(example_1);
-    mp_embed_exec_str(example_2);
+    // printf("here\n"); fflush(stdout);
+    // mp_embed_exec_str(example_2);
 
     // Deinitialise MicroPython.
     mp_embed_deinit();
